0bf8288ac0e919cd5ecb7437e0dcd53a24d47db7
samuel <eng.samuel.khalaf@gmail.com>
2025-06-19 20:57:12 +0300

    fix media and document preview

--- resources/views/admin/workshops/modals/preview.blade.php
+++ resources/views/admin/workshops/modals/preview.blade.php
@@ -5,10 +5,12 @@
             <div class="modal-header">
                 <h2 class="fw-bold" id="preview-title">Preview</h2>
                 <div class="btn btn-icon btn-sm btn-active-icon-primary" data-kt-users-modal-action="close">
-                    <i class="ki-duotone ki-cross fs-1">
-                        <span class="path1"></span>
-                        <span class="path2"></span>
-                    </i>
+                    <span class="svg-icon svg-icon-1">
+                        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <rect opacity="0.5" x="6" y="17.3137" width="16" height="2" rx="1" transform="rotate(-45 6 17.3137)" fill="currentColor" />
+                            <rect x="7.41422" y="6" width="16" height="2" rx="1" transform="rotate(45 7.41422 6)" fill="currentColor" />
+                        </svg>
+                    </span>
                 </div>
             </div>
             <div class="modal-body scroll-y mx-5 mx-xl-15 my-7">
